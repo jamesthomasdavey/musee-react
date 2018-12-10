66931c56afff0db2a9a9ec61c9703500825d39f4
James Davey <james.t.davey@gmail.com>
2018-12-10 14:42:54 -0800

    finish css slideshow but will probably fix it due to decreased performance

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -9,7 +9,32 @@ class Landing extends Component {
     return (
       <div className={classes.wrapper}>
         <div className={classes.innerWrapper}>
-          {/* <div className={classes.overlay} /> */}
+          <ul className={classes.slideshow}>
+            <li>
+              <span>Image 01</span>
+            </li>
+            <li>
+              <span>Image 02</span>
+            </li>
+            <li>
+              <span>Image 03</span>
+            </li>
+            <li>
+              <span>Image 04</span>
+            </li>
+            <li>
+              <span>Image 05</span>
+            </li>
+            <li>
+              <span>Image 06</span>
+            </li>
+            <li>
+              <span>Image 07</span>
+            </li>
+            <li>
+              <span>Image 08</span>
+            </li>
+          </ul>
         </div>
       </div>
     );
