b42a41347e468e86b2555247c4922f03705ba943
James Thomas Davey <jamesthomasdavey@james-air.lan>
2021-12-01 16:23:59 -0800

    remove covid modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // package
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // layout component
-import Navigation from './components/layout/Navigation/Navigation';
-import Footer from './components/layout/Footer/Footer';
+import Navigation from "./components/layout/Navigation/Navigation";
+import Footer from "./components/layout/Footer/Footer";
 // import ScrollToTop from './components/layout/ScrollToTop/ScrollToTop';
-import Modal from './components/layout/Modal/Modal';
+import Modal from "./components/layout/Modal/Modal";
 
 // page component
-import Landing from './components/pages/Landing/Landing';
-import Visit from './components/pages/Visit/Visit';
-import Story from './components/pages/Story/Story';
-import Gallery from './components/pages/Gallery/Gallery';
-import Shop from './components/pages/Shop/Shop';
-import Contact from './components/pages/Contact/Contact';
-import ShopItem from './components/pages/ShopItem/ShopItem';
+import Landing from "./components/pages/Landing/Landing";
+import Visit from "./components/pages/Visit/Visit";
+import Story from "./components/pages/Story/Story";
+import Gallery from "./components/pages/Gallery/Gallery";
+import Shop from "./components/pages/Shop/Shop";
+import Contact from "./components/pages/Contact/Contact";
+import ShopItem from "./components/pages/ShopItem/ShopItem";
 
 // css
-import classes from './App.module.css';
+import classes from "./App.module.css";
 
 class App extends Component {
   render() {
@@ -28,18 +28,18 @@ class App extends Component {
       >
         {/* <ScrollToTop> */}
         <React.Fragment>
-          <Modal />
+          {/* <Modal /> */}
           <div className={classes.wrapper}>
             <div className={classes.content}>
               <Navigation />
               <Switch>
-                <Route exact path='/' component={Landing} />
-                <Route exact path='/visit' component={Visit} />
-                <Route exact path='/story' component={Story} />
-                <Route exact path='/gallery' component={Gallery} />
-                <Route exact path='/shop' component={Shop} />
-                <Route exact path='/shop/:shopItem' component={ShopItem} />
-                <Route exact path='/contact' component={Contact} />
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/visit" component={Visit} />
+                <Route exact path="/story" component={Story} />
+                <Route exact path="/gallery" component={Gallery} />
+                <Route exact path="/shop" component={Shop} />
+                <Route exact path="/shop/:shopItem" component={ShopItem} />
+                <Route exact path="/contact" component={Contact} />
               </Switch>
             </div>
             <Footer />
--- src/components/pages/Visit/Visit.js
+++ src/components/pages/Visit/Visit.js
@@ -1,76 +1,70 @@
 // package
-import React, { Component } from 'react';
-import { HashLink } from 'react-router-hash-link';
+import React, { Component } from "react";
+import { HashLink } from "react-router-hash-link";
 
 // component
-import Map from './../../layout/Map/Map';
-import About from './components/About/About';
+import Map from "./../../layout/Map/Map";
+import About from "./components/About/About";
 
 // css
-import classes from './Visit.module.css';
+import classes from "./Visit.module.css";
 
 // img
-import openCorner from './img/openCorner.svg';
+import openCorner from "./img/openCorner.svg";
 
 class Visit extends Component {
   render() {
-    document.title = 'Visit | Musée Mécanique';
+    document.title = "Visit | Musée Mécanique";
     return (
       <div className={classes.wrapper}>
-        <h2 className='sr-only'>Visit</h2>
+        <h2 className="sr-only">Visit</h2>
         <div className={classes.infoWrapper}>
-          <div className='ui container'>
+          <div className="ui container">
             <div className={classes.infoHeaderWrapper}>
-              <img alt='' src={openCorner} className={classes.openLeftCorner} />
+              <img alt="" src={openCorner} className={classes.openLeftCorner} />
               <img
-                alt=''
+                alt=""
                 src={openCorner}
                 className={classes.openRightCorner}
               />
-              <h3 className={classes.infoOpenDays}>
-                We have reopened! 🎊
-              </h3>
-              <p className={classes.infoCost}>
-                Once again, we are open 365 days a year!
-                <br />
-                Admission is free!
-              </p>
+              <h3 className={classes.infoOpenDays}>Open 365 Days a Year</h3>
+              <p className={classes.infoCost}>Admission is free!</p>
               <div className={classes.icons}>
-                <HashLink to='/contact#send-a-message'>
-                  <i aria-hidden='true' className='envelope outline icon' />
-                  <span className='sr-only'>Send a message</span>
+                <HashLink to="/contact#send-a-message">
+                  <i aria-hidden="true" className="envelope outline icon" />
+                  <span className="sr-only">Send a message</span>
                 </HashLink>
                 <a
-                  href='https://www.facebook.com/museemecaniquesf/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.facebook.com/museemecaniquesf/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i aria-hidden='true' className='facebook icon' />
-                  <span className='sr-only'>Facebook</span>
+                  <i aria-hidden="true" className="facebook icon" />
+                  <span className="sr-only">Facebook</span>
                 </a>
                 <a
-                  href='https://www.instagram.com/explore/locations/117557/musee-mecanique/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.instagram.com/explore/locations/117557/musee-mecanique/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i aria-hidden='true' className='instagram icon' />
-                  <span className='sr-only'>Instagram</span>
+                  <i aria-hidden="true" className="instagram icon" />
+                  <span className="sr-only">Instagram</span>
                 </a>
                 <a
-                  href='https://www.google.com/search?rlz=1C5CHFA_enUS725US725&ei=1hggXJqACMz0rAGnsY_QBQ&q=Mus%C3%A9e+M%C3%A9canique&oq=Mus%C3%A9e+M%C3%A9canique&gs_l=psy-ab.3..35i39l2j0j0i67j0l2j0i22i30l4.36489.43079..44589...4.0..0.319.3455.1j14j5j1....2..0....1..gws-wiz.......38j0i71j0i131i67j0i131j0i22i10i30.s3CJM2eXixM#lrd=0x808580e254330009:0xed9aa8b90062eab8,1,,,'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.google.com/search?rlz=1C5CHFA_enUS725US725&ei=1hggXJqACMz0rAGnsY_QBQ&q=Mus%C3%A9e+M%C3%A9canique&oq=Mus%C3%A9e+M%C3%A9canique&gs_l=psy-ab.3..35i39l2j0j0i67j0l2j0i22i30l4.36489.43079..44589...4.0..0.319.3455.1j14j5j1....2..0....1..gws-wiz.......38j0i71j0i131i67j0i131j0i22i10i30.s3CJM2eXixM#lrd=0x808580e254330009:0xed9aa8b90062eab8,1,,,"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i aria-hidden='true' className='google icon' />
-                  <span className='sr-only'>Google</span>
+                  <i aria-hidden="true" className="google icon" />
+                  <span className="sr-only">Google</span>
                 </a>
                 <a
-                  href='https://www.yelp.com/biz/mus%C3%A9e-m%C3%A9canique-san-francisco-4'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.yelp.com/biz/mus%C3%A9e-m%C3%A9canique-san-francisco-4"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i aria-hidden='true' className='yelp icon' />
-                  <span className='sr-only'>Yelp</span>
+                  <i aria-hidden="true" className="yelp icon" />
+                  <span className="sr-only">Yelp</span>
                 </a>
               </div>
             </div>
@@ -78,7 +72,7 @@ class Visit extends Component {
           <div className={classes.mapAddressHoursWrapper}>
             <div className={classes.mapAddressHoursWrapperInner}>
               <div className={classes.map}>
-                <h3 className='sr-only'>Map</h3>
+                <h3 className="sr-only">Map</h3>
                 <Map />
               </div>
               <div className={classes.addressHoursWrapper}>
@@ -112,7 +106,7 @@ class Visit extends Component {
         <div className={classes.bottomWrapper}>
           <div className={classes.spacer} />
           <div className={classes.bottomWrapperInner}>
-            <div className='ui text container'>
+            <div className="ui text container">
               <div className={classes.aboutWrapper}>
                 <About />
               </div>
