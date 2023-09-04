9436bac045e0bbc588337147d016e90cec489023
James Davey <james.davey@fastmail.com>
2023-09-04 11:58:40 -0700

    fix scroll behavior again

--- src/components/layout/ScrollToTop/ScrollToTop.js
+++ src/components/layout/ScrollToTop/ScrollToTop.js
@@ -3,11 +3,17 @@ import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
-    // if (this.props.location.pathname !== prevProps.location.pathname) {
-    //   document
-    //     .querySelector(".Navigation_headerWrapper__1l5L_")
-    //     .scrollIntoView();
-    // }
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      document.body.scrollTo({
+        top: 0,
+        left: 0,
+      });
+    }
+    if (this.props.location.pathname.substr("/shop#")) {
+      setTimeout(() => {
+        document.body.scrollBy(0, -100);
+      }, 0);
+    }
   }
   render() {
     return <Fragment>{this.props.children}</Fragment>;
