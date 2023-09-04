0355103b0db2403f3ec4880a8ae132bdfe2424f5
James Davey <james.davey@fastmail.com>
2023-09-04 12:23:01 -0700

    add bandcamp link

--- src/components/layout/Navigation/components/CollapsibleMenu/components/MenuItems/components/MenuItem/MenuItem.js
+++ src/components/layout/Navigation/components/CollapsibleMenu/components/MenuItems/components/MenuItem/MenuItem.js
@@ -1,13 +1,13 @@
 // package
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 
 // ext component
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // css
-import classes from './MenuItem.module.css';
+import classes from "./MenuItem.module.css";
 
 const MenuItem = (props) => {
   const navLinkHandler = () => {
@@ -19,27 +19,27 @@ const MenuItem = (props) => {
     isActive = true;
   }
   const tabHandler = (e) => {
-    if (props.name === 'Facebook') {
-      if (e.key === 'Tab' && !e.shiftKey) {
+    if (props.name === "Facebook") {
+      if (e.key === "Tab" && !e.shiftKey) {
         props.closeNav();
       }
     }
   };
-  if (props.name === 'Facebook') {
+  if (props.name === "Facebook") {
     return (
       <a
         className={[
           classes.wrapper,
-          props.isInverted ? classes.inverted : '',
-          isActive ? classes.active : '',
-        ].join(' ')}
-        href='https://www.facebook.com/museemecaniquesf/'
+          props.isInverted ? classes.inverted : "",
+          isActive ? classes.active : "",
+        ].join(" ")}
+        href="https://www.facebook.com/museemecaniquesf/"
         onKeyDown={tabHandler}
-        target='_blank'
-        rel='noopener noreferrer'
+        target="_blank"
+        rel="noopener noreferrer"
       >
         <span className={classes.text}>{props.name}</span>
-        {isActive && <span className='sr-only'>- current</span>}
+        {isActive && <span className="sr-only">- current</span>}
       </a>
     );
   }
@@ -47,15 +47,15 @@ const MenuItem = (props) => {
     <Link
       className={[
         classes.wrapper,
-        props.isInverted ? classes.inverted : '',
-        isActive ? classes.active : '',
-      ].join(' ')}
+        props.isInverted ? classes.inverted : "",
+        isActive ? classes.active : "",
+      ].join(" ")}
       to={props.url}
       onClick={navLinkHandler}
       onKeyDown={tabHandler}
     >
       <span className={classes.text}>{props.name}</span>
-      {isActive && <span className='sr-only'>- current</span>}
+      {isActive && <span className="sr-only">- current</span>}
     </Link>
   );
 };
--- src/components/pages/Shop/Shop.js
+++ src/components/pages/Shop/Shop.js
@@ -1,28 +1,44 @@
 // package
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // data
-import shopItems from './../../../data/shopItems';
+import shopItems from "./../../../data/shopItems";
 
 // component
-import ShopItem from './components/ShopItem/ShopItem';
+import ShopItem from "./components/ShopItem/ShopItem";
 
 // css
-import classes from './Shop.module.css';
+import classes from "./Shop.module.css";
 
 class Shop extends Component {
   render() {
-    document.title = 'Shop | Musée Mécanique';
+    document.title = "Shop | Musée Mécanique";
     const shopItemsList = shopItems.map((shopItem) => (
       <ShopItem key={shopItem.name} shopItem={shopItem} />
     ));
     return (
-      <div className={classes.wrapper}>
-        <h2 className='sr-only'>Shop</h2>
-        <div className='ui container'>
-          <div className='ui divided items'>{shopItemsList}</div>
+      <React.Fragment>
+        <div className={[classes.bandcampBanner].join(" ")}>
+          <a
+            className={classes.bandcampLink}
+            href="https://museemecaniquesf.bandcamp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            👉{" "}
+            <span className={classes.linkText}>
+              Digital versions of our CDs are now available on Bandcamp!
+            </span>{" "}
+            👉
+          </a>
         </div>
-      </div>
+        <div className={classes.wrapper}>
+          <h2 className="sr-only">Shop</h2>
+          <div className="ui container">
+            <div className="ui divided items">{shopItemsList}</div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
