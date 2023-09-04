9d5188aa7333afb14abcf5cf4aa756ab1060b8b6
James Davey <james.davey@fastmail.com>
2023-09-04 11:51:20 -0700

    reverted scroll behavior because it broke prod

--- src/components/layout/ScrollToTop/ScrollToTop.js
+++ src/components/layout/ScrollToTop/ScrollToTop.js
@@ -3,16 +3,11 @@ import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
-      document
-        .querySelector(".Navigation_headerWrapper__1l5L_")
-        .scrollIntoView();
-    }
-    if (this.props.location.pathname.substr("/shop#")) {
-      setTimeout(() => {
-        window.scrollBy(0, 100);
-      }, 300);
-    }
+    // if (this.props.location.pathname !== prevProps.location.pathname) {
+    //   document
+    //     .querySelector(".Navigation_headerWrapper__1l5L_")
+    //     .scrollIntoView();
+    // }
   }
   render() {
     return <Fragment>{this.props.children}</Fragment>;
